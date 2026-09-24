31100d3bc2b00d530ec2a257ed32768f213e4147
harinireddyyelala-stack <harinireddyyelala@gmail.com>
2026-09-24 12:14:12 +0530

    Day-4: chatBot with memory

---
+++ Day-4/chatBot.py
@@ -0,0 +1,25 @@
+import ollama
+msgs = [
+    {"role": "system",
+     "content": "Give the answer in simple terms."}
+]
+while True:
+    question = input("Ask the question: ")
+    if question.lower() == "exit":
+        break
+    msgs.append(
+        {"role": "user",
+         "content": question}
+    )
+
+    response = ollama.chat(
+        model="llama3.2:3b",
+        messages=msgs)
+    msgs.append(
+          {"role":"assistant",
+           "content": response["message"]["content"]}
+    )
+    print("AI:", response["message"]["content"])
+print("----Chat History----\n")
+for msg in msgs:
+    print(msg["role"],":",msg["content"])
